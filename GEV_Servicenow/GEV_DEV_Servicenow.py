ff84c985a756c7ce0d1ae71cd5e38d2ade8dfcc0
abhishekkonaml <abhishek.kona@microland.com>
2025-05-14 17:00:45 +0530

    Update GEV_DEV_Servicenow.py

--- GEV_Servicenow/GEV_DEV_Servicenow.py
+++ GEV_Servicenow/GEV_DEV_Servicenow.py
@@ -124,7 +124,26 @@ class Incidents:
           except Exception as e:
                   return ("Failed - Something went wrong - {}".format(str(e)))
 
+class CMDB:
+      def __init__(self):
+        url = "https://fssfed.stage.ge.com/fss/as/token.oauth2?grant_type=client_credentials&scope=api"
+        headers = {
+          'Authorization': 'Basic ckdnMVd4V280Rko2SnpZZ2lUY3VnU1pqVzEzdmk1UVBjYnZjTnUxTUF0ZkZRM0xBOjA5enR1UUk4NlZMcVV5TEFOYW5taERuNGRaQXc1UTlYeENkSE5Pb0FpcTAxd0lOTU5JMTdDZFVibXMwRzRNMXQ=',
+          
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Cookie': 'PF=AvKeI0z1JjyimQVUejoyav'
 
+        }
+        
+        try: 
+           response = requests.request("POST", url, headers=headers, verify=False)
+           if response.status_code == 200:
+              self.access_token=response.json()["access_token"]
+           else: 
+              self.access_token="Failed to fetch the access token"
+        except Exception as e:
+              self.access_token="Failed to fetch the access token-{}".format(str(e))
 
 incobj=Incidents()
 query1="number={}?sso={}".format("GEVINC0016024","503437104")
@@ -145,6 +164,6 @@ work_notes="This is a test. This is only a test."
 
 close_notes="Closed/Resolved by Caller"
 close_code = "Duplicate"
-print(incobj.close_ticket("GEVINC0017971",close_notes,close_code))
+print(incobj.close_ticket("GEVINC0017969",close_notes,close_code))
 
         
