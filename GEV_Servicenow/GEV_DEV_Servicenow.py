fcb601b82f792902407787e3208ac58931dae366
abhishekkonaml <abhishek.kona@microland.com>
2025-05-14 17:39:09 +0530

    Update GEV_DEV_Servicenow.py

--- GEV_Servicenow/GEV_DEV_Servicenow.py
+++ GEV_Servicenow/GEV_DEV_Servicenow.py
@@ -144,8 +144,41 @@ class CMDB:
               self.access_token="Failed to fetch the access token"
         except Exception as e:
               self.access_token="Failed to fetch the access token-{}".format(str(e))
+      def get_ci_details(self,hostname):
+          url="https://stage.api.gevernova.com/servicenow_task_cmdb/name/{}".format(hostname)
+          token='Bearer {}'.format(self.access_token)
+          headers = {
+               'Authorization': token,
+               'tradingPartner': 'com.microland.microwhiz',
+               'Content-Type': 'application/json'
+              }
+          try: 
+               response = requests.request("GET", url, headers=headers,verify=False)
+               if response.status_code == 200:
+                  return (response.json()['result'])
+               else:
+                  return ("Failed - {}".format(response.json()['error']['message']))
+          except Exception as e:
+                  return ("Failed - Something went wrong - {}".format(str(e)))
+      def get_ci_full_details(self,hostname):
+          url="https://stage.api.gevernova.com/servicenow_task_cmdb/full/{}".format(hostname)
+          token='Bearer {}'.format(self.access_token)
+          headers = {
+               'Authorization': token,
+               'tradingPartner': 'com.microland.microwhiz',
+               'Content-Type': 'application/json'
+              }
+          try: 
+               response = requests.request("GET", url, headers=headers,verify=False)
+               if response.status_code == 200:
+                  return (response.json()['result'])
+               else:
+                  return ("Failed - {}".format(response.json()['error']['message']))
+          except Exception as e:
+                  return ("Failed - Something went wrong - {}".format(str(e)))
 
-incobj=Incidents()
+
+#incobj=Incidents()
 query1="number={}?sso={}".format("GEVINC0016024","503437104")
 query2="active=true^assignment_groupGE Vernova DT CTO Network?sso={}".format("503437104")
 
@@ -162,8 +195,11 @@ work_notes="This is a test. This is only a test."
 #print(incobj.incident_creation(opened_by,caller_id,business_service,service_offering,assignment_group,short_description,description,urgency,work_notes))
 #print(incobj.update_notes("GEVINC0017971",work_notes))
 
-close_notes="Closed/Resolved by Caller"
-close_code = "Duplicate"
-print(incobj.close_ticket("GEVINC0017970",close_notes,close_code))
+#close_notes="Closed/Resolved by Caller"
+#close_code = "Duplicate"
+#print(incobj.close_ticket("GEVINC0017970",close_notes,close_code))
+
+cmdbobj=CMDB()
+print(cmdbobj.get_ci_details("crpwcedzalgie91"))
 
         
