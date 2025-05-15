d700e02ebc20e2f74fdfc44b1f13dc7f49b7a852
abhishekkonaml <abhishek.kona@microland.com>
2025-05-15 22:13:30 +0530

    Update GEV_Prod_Servicenow.py

--- GEV_Servicenow/GEV_Prod_Servicenow.py
+++ GEV_Servicenow/GEV_Prod_Servicenow.py
@@ -233,8 +233,8 @@ work_notes="This is a test. This is only a test."
 
 cmdbobj=CMDB()
 #print(cmdbobj.access_token)
-print(cmdbobj.get_ci_details("crpwcedzalgie91"))
-#print(cmdbobj.get_ci_full_details("crpwcedzalgie91"))
+#print(cmdbobj.get_ci_details("crpwcedzalgie91"))
+print(cmdbobj.get_ci_full_details("crpwcedzalgie91"))
 #print(cmdbobj.get_ci_details_by_id("1004209415"))
 #field=owned_by&value=Korsós, Balázs
 #query2="field=owned_by&value={}".format("Korsós, Balázs")
