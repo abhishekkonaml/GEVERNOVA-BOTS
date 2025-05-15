60a7b24b3af49684bbd1250965f62e0cd8398607
abhishekkonaml <abhishek.kona@microland.com>
2025-05-15 22:34:02 +0530

    Update GEV_Prod_Servicenow.py

--- GEV_Servicenow/GEV_Prod_Servicenow.py
+++ GEV_Servicenow/GEV_Prod_Servicenow.py
@@ -226,8 +226,8 @@ urgency="3"
 work_notes="This is a test. This is only a test."
 #print(incobj.get_incident_details_by_query(query1))
 #print(incobj.get_incident_details_by_query(query2))
-print(incobj.incident_creation(opened_by,caller_id,business_service,service_offering,assignment_group,short_description,description,urgency,work_notes))
-#print(incobj.update_notes("GEVINC0017971",work_notes))
+#print(incobj.incident_creation(opened_by,caller_id,business_service,service_offering,assignment_group,short_description,description,urgency,work_notes))
+print(incobj.update_notes("GEVINC0018081",work_notes))
 
 #close_notes="Closed/Resolved by Caller"
 #close_code = "Duplicate"
