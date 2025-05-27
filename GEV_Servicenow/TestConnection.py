5249e48c92d5a05acac4676a4f713a0b5ee94097
abhishekkonaml <abhishek.kona@microland.com>
2025-05-27 18:06:45 +0530

    testing

--- GEV_Servicenow/TestConnection.py
+++ GEV_Servicenow/TestConnection.py
@@ -2,26 +2,60 @@ import paramiko
 import decoding1
 import sys
 import time
-hostname=sys.argv[1]
+import pandas as pd
+from multiprocessing import Pool
+import csv
+df=pd.read_csv('hostnames.csv')
+hostnames=df['Hostname'].to_list()
 
 
-try:
-    cisco_username=decoding1.decoding1('NTAxNTI5NTI4')
-    cisco_password=decoding1.decoding1('V2hpejIwMTJDMTBzZQ==')
 
-    ChartserverConnection = paramiko.SSHClient()
-    ChartserverConnection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ChartserverConnection.connect(hostname=hostname, username=cisco_username,password=cisco_password)
-    channel = ChartserverConnection.invoke_shell()
-    out= channel.recv(10000000000000000) 
-    command="show clock"  
-    channel.send(command+ '\n')
-    time.sleep(5)
-    out= channel.recv(10000000000000000)
-    result=str(out.decode()).replace(command,"")
+def test_connection(hostname):
+    try:
+        cisco_username=decoding1.decoding1('NTAxNTI5NTI4')
+        cisco_password=decoding1.decoding1('V2hpejIwMTJDMTBzZQ==')
+        hostname=hostname+".gdn.ge.com"
+        ChartserverConnection = paramiko.SSHClient()
+        ChartserverConnection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ChartserverConnection.connect(hostname=hostname, username=cisco_username,password=cisco_password)
+        channel = ChartserverConnection.invoke_shell()
+        out= channel.recv(10000000000000000) 
+        command="show clock"  
+        channel.send(command+ '\n')
+        time.sleep(5)
+        out= channel.recv(10000000000000000)
+        result=str(out.decode()).replace(command,"")
+        
+        ChartserverConnection.close()
+        print(result)
+        result57={'Hostname': hostname, 'Result': 'Success'}
     
-    ChartserverConnection.close()
-    print(result)
-except Exception as e:
-    ChartserverConnection.close()
-    print("Failed - Error! - {}".format(str(e)))
+            
+        with open("Outcome.csv", "a", newline="") as f:
+             w = csv.DictWriter(f, result57.keys())
+             w.writerow(result57)
+    except Exception as e:
+        ChartserverConnection.close()
+        print("Failed - Error! - {}".format(str(e)))
+        result57={'Hostname': hostname, 'Result': 'Failed'}
+        with open("Outcome.csv", "a", newline="") as f:
+             w = csv.DictWriter(f, result57.keys())
+             w.writerow(result57)
+
+
+import csv
+
+my_dict = {'Hostname': '','Result': ''}
+
+with open("ManualIntervention.csv", "w", newline="") as f:
+    w = csv.DictWriter(f, my_dict.keys())
+    w.writeheader()
+    w.writerow(my_dict)
+
+
+def pool_handler():
+    p = Pool(10)
+    p.map(test_connection, hostnames)
+    
+pool_handler()
+
