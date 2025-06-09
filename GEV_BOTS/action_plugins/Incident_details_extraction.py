221e90eca3ea0270e209ddbeeaa923de1ce25e97
abhishekkonaml <abhishek.kona@microland.com>
2025-06-09 14:50:58 +0530

    bot

--- GEV_BOTS/action_plugins/Incident_details_extraction.py
+++ GEV_BOTS/action_plugins/Incident_details_extraction.py
@@ -24,10 +24,10 @@ class ActionModule(ActionBase):
             incobj=Incidents()
             incident_details=incobj.get_incident_details_by_query(query)
             if type(incident_details)==list:
-               description=incident_details[0].keys()
-               print("===="*50)
-               print("Description: ",description)
-               print("===="*50)
+               description=incident_details[0]['description']
+               #print("===="*50)
+               #print("Description: ",description)
+               #print("===="*50)
                #Bot Classification Logic
                
                # Usecase-1: Connection Down Bot 
