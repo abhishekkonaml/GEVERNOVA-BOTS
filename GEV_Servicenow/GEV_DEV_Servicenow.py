facd9b93d852e532bc28e907082f0e24c4275225
abhishekkonaml <abhishek.kona@microland.com>
2025-06-09 16:26:51 +0530

    bot

--- GEV_Servicenow/GEV_DEV_Servicenow.py
+++ GEV_Servicenow/GEV_DEV_Servicenow.py
@@ -247,12 +247,12 @@ work_notes="This is a test. This is only a test."
 cmdb_ci='EMTsaEFRmontp05'
 #print(incobj.get_incident_details_by_query(query1))
 #print(incobj.get_incident_details_by_query(query2))
-print(incobj.incident_creation(opened_by,caller_id,business_service,service_offering,assignment_group,short_description,description,urgency,work_notes,cmdb_ci))
+#print(incobj.incident_creation(opened_by,caller_id,business_service,service_offering,assignment_group,short_description,description,urgency,work_notes,cmdb_ci))
 #print(incobj.update_notes("GEVINC0017971",work_notes))
 
-#close_notes="Closed/Resolved by Caller"
-#close_code = "Duplicate"
-#print(incobj.close_ticket("GEVINC0017970",close_notes,close_code))
+close_notes="Closed/Resolved by Caller"
+close_code = "Duplicate"
+print(incobj.close_ticket("GEVINC0017970",close_notes,close_code))
 
 #cmdbobj=CMDB()
 #print(cmdbobj.get_ci_details("crpwcedzalgie91"))
