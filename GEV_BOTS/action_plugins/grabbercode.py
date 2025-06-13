76d1ac9bcd592760c2594b73723c3905918a1a7d
abhishekkonaml <abhishek.kona@microland.com>
2025-06-13 21:32:10 +0530

    Update grabbercode.py

--- GEV_BOTS/action_plugins/grabbercode.py
+++ GEV_BOTS/action_plugins/grabbercode.py
@@ -2,12 +2,10 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 from ansible.plugins.action import ActionBase
 import warnings
-#from BtoBtransaction import GrabIncidents
+import BtoBtransaction
+from BtoBtransaction import GrabIncidents
 warnings.filterwarnings("ignore")
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-print("Current Python sys.path:", sys.path)
+
 
 class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
