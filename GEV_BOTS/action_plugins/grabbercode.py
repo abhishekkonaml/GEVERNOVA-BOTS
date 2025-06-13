d434d23226171b9082e235da4510c0e7447fb284
abhishekkonaml <abhishek.kona@microland.com>
2025-06-13 21:36:14 +0530

    Update grabbercode.py

--- GEV_BOTS/action_plugins/grabbercode.py
+++ GEV_BOTS/action_plugins/grabbercode.py
@@ -2,7 +2,9 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 from ansible.plugins.action import ActionBase
 import warnings
-import BtoBtransaction
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from BtoBtransaction import GrabIncidents
 warnings.filterwarnings("ignore")
 
