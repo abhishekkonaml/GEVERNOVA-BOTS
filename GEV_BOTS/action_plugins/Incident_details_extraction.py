869fa5ae9a59d468db6b2ca9f6be8fd23630c278
abhishekkonaml <abhishek.kona@microland.com>
2025-07-09 13:51:29 +0530

    Update Incident_details_extraction.py

--- GEV_BOTS/action_plugins/Incident_details_extraction.py
+++ GEV_BOTS/action_plugins/Incident_details_extraction.py
@@ -20,8 +20,6 @@ class ActionModule(ActionBase):
             incident_no=self._task.args["incident_no"]
             #host_pattern='Host: (.*)'
             #interface_pattern="Datasource: Network Interfaces-(.*) "
-            host_pattern="host (.*) is experiencing"
-            interface_pattern="packets on (.*) \["
             
             
             incobj=Incidents()
@@ -34,7 +32,11 @@ class ActionModule(ActionBase):
                #Bot Classification Logic
                
                # Usecase-1: Connection Down Bot 
-               if 'network interface' in description.lower(): 
+               if 'network interface' in description.lower() and "statusflap" in description.lower(): 
+                  #host_pattern="host (.*) is experiencing"
+                  host_pattern="- (.*)Network Interfaces"
+                  #interface_pattern="packets on (.*) \["
+                  interface_pattern="Network Interfaces-(.*) \["
                   hostname=re.findall(host_pattern,description)
                   interface=re.findall(interface_pattern,description)
                   if len(hostname)>0:
