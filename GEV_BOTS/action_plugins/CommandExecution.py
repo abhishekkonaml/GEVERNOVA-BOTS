6ff8525d5cc207999fd2499b4cc99d31af66e67e
abhishekkonaml <abhishek.kona@microland.com>
2025-07-09 23:01:44 +0530

    Update CommandExecution.py

--- GEV_BOTS/action_plugins/CommandExecution.py
+++ GEV_BOTS/action_plugins/CommandExecution.py
@@ -57,10 +57,7 @@ class ActionModule(ActionBase):
                 result = 'Unable to execute show log command'
                 print(result)
                 exit()
-            elif(('>' not in str(out.decode())) or ('#' not in str(out.decode()))):
-                result = 'SSH Failed'
-                print(result)
-                exit()
+            
 
             result=str(out.decode()).replace(command,"")
             result=result.lower().replace(hostname1.lower(),"")
