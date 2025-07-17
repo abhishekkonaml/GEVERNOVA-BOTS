00593765f7ad23c3c3a6c43a5737f05a66a23df2
abhishekkonaml <abhishek.kona@microland.com>
2025-07-17 13:57:36 +0530

    Update grabbercode.py

--- GEV_BOTS/action_plugins/grabbercode.py
+++ GEV_BOTS/action_plugins/grabbercode.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from BtoBtransaction import GrabIncidents
 from GEV_DEV_Servicenow import Incidents
 from awx_trigger import AWX_Trigger
-warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore") 
 
 
 class ActionModule(ActionBase):
