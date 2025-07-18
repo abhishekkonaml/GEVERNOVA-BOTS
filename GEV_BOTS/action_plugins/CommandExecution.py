317d923b8d2653c7e885fd44ba7750855bba85d9
aishwarya-shet <aishwarya.shet@microland.com>
2025-07-18 15:29:11 +0530

    Update CommandExecution.py

--- GEV_BOTS/action_plugins/CommandExecution.py
+++ GEV_BOTS/action_plugins/CommandExecution.py
@@ -64,6 +64,9 @@ class ActionModule(ActionBase):
             
 
             result=str(out.decode()).replace(command,"")
+            if(command == 'sh ip in brief'):
+                lines=result.splitlines()
+                result ="\n".join(lines[:4])
             result=result.lower().replace(hostname1.lower(),"")
             result=result.replace('>',"")
             #result=result.replace('\r\n',"")
