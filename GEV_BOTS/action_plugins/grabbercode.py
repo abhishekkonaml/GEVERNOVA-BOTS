af18ef1992b5738ff215b0ef20b9b8dc4d055b1b
abhishekkonaml <abhishek.kona@microland.com>
2025-07-24 13:23:45 +0530

    grabbercode changes

--- GEV_BOTS/action_plugins/grabbercode.py
+++ GEV_BOTS/action_plugins/grabbercode.py
@@ -26,21 +26,21 @@ class ActionModule(ActionBase):
            incidents=[]
            url = awx_url+"job_templates/12/launch/"
            for incident in grabincidents:
-               if "New" in incident['Payload']['RequestInfo']['state']:
-                   if "statusflap" in incident['Payload']['RequestInfo']['description'].lower():
-                       pass
-                       #incidents.append(incident['Payload']['RequestInfo']['number'])
-                   elif "down" in incident['Payload']['RequestInfo']['description'].lower():
+               if "statusflap" in incident['Payload']['RequestInfo']['description'].lower():
+                    state_inc=incobj.get_incident_details_by_incident_number(incident['Payload']['RequestInfo']['number'])
+                    if "New" in state_inc[0]['state']:
                        incidents.append(incident['Payload']['RequestInfo']['number'])
-                       
+               elif "down" in incident['Payload']['RequestInfo']['description'].lower():
+                       #incidents.append(incident['Payload']['RequestInfo']['number'])
+                       pass
            print(incidents)            
            #incidents=[incident['Payload']['RequestInfo']['number'] for incident in grabincidents if "network interfaces" in incident['Payload']['RequestInfo']['description'].lower() and "statusflap" in incident['Payload']['RequestInfo']['description'].lower() and "new" in incident['Payload']['RequestInfo']['state'].lower()]
            #incidents=["GEVINC0029674","GEVINC0029678"]
            awxobj=AWX_Trigger()
-           print(incobj.assigned_to_bot(incidents[0]))
-           triggering_template=awxobj.trigger(url,awx_uname,awx_pass,incidents[0])
-           print(triggering_template)
-           print("="*50)
+           #print(incobj.assigned_to_bot(incidents[0]))
+           #triggering_template=awxobj.trigger(url,awx_uname,awx_pass,incidents[0])
+           #print(triggering_template)
+           #print("="*50)
            return {'status': 'success','result': incidents}
         except Exception as e:
             return {'status': 'failed', 'result': str(e)}
