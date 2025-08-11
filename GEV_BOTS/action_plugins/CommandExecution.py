23d39e0764785fb9b7e10bb1a6cf63c995b43311
aishwarya-shet <aishwarya.shet@microland.com>
2025-08-11 13:46:50 +0530

    Update CommandExecution.py

--- GEV_BOTS/action_plugins/CommandExecution.py
+++ GEV_BOTS/action_plugins/CommandExecution.py
@@ -68,9 +68,12 @@ class ActionModule(ActionBase):
                 lines=result.splitlines()
                 result ="\n".join(lines[:8])
             result=result.lower().replace(hostname1.lower(),"")
-            result=result.lower().replace('\\r\\n'+hostname1.lower(),"")
+            if(command == 'show clock'):
+                result=result.lower().replace('\\r\\n'+hostname1.lower(),"")
+                result=result.replace('\\r\\n',"")
+                print('replcement done')
             result=result.replace('>',"")
-            result=result.replace('\\r\\n',"")
+            
             
             ChartserverConnection.close()
             if 'no' not in notesupdate:
