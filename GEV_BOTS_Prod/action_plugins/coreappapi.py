f6d1bf6dc59e1b8754d5afa1fe28c3c836717589
Abhishek Kona <abhishek.kona@microland.com>
2025-10-09 18:21:27 +0530

    Update coreappapi.py

--- GEV_BOTS_Prod/action_plugins/coreappapi.py
+++ GEV_BOTS_Prod/action_plugins/coreappapi.py
@@ -17,16 +17,19 @@ class CoreApp:
             "description": desc,
             "priorityName": "P3",
             "deviceName": "",
-            "application_owner": org_assignment_group
+            "application_owner": org_assignment_group.split("\n\n")[0]
           }
         })
+        print("===="*10)
         print(payload)
+        print("===="*10)
         headers = {
           'Content-Type': 'application/json',
           'AuthToken': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODM5NzkyZTJkODIzOTVjYWFkZGRlNzkiLCJlbWFpbCI6ImlnYm90c2FwaXNhZG1pbkBnZXZlcm5vdmEuY29tIiwidGVuYW50IjoiZ2VfdmVybm92YSIsImlhdCI6MTc1NTUyMTI3OCwiZXhwIjoxNzg3MDU3Mjc4fQ.DGIGAMkcAYpxWBe9R-kmZPiZGgI2zH4vyn8iyN8aHAE'
         }
         try:
             response = requests.request("POST", url, headers=headers, data=payload,verify=False,proxies=proxies)
+            print("----------------------------",response.status_code,"-----------------------------")
             if response.status_code==201:
                return response.json()
             else:
