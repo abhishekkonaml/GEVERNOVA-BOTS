57223a328da26c3542177d322ed3121c9e11a980
Abhishek Kona <abhishek.kona@microland.com>
2025-10-09 22:45:54 +0530

    test

--- GEV_BOTS_Prod/action_plugins/sample.py
+++ GEV_BOTS_Prod/action_plugins/sample.py
@@ -1,7 +1,21 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+from ansible.plugins.action import ActionBase
+import warnings
 import os
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-current_path=os.getcwd()+"\\failedcases.txt"
+warnings.filterwarnings("ignore") 
 
-with open(current_path,'w') as file:
-    file.write(str({'status':'success'})+"\n")
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+        super(ActionModule, self).run(tmp, task_vars)
+        try: 
+           current_path=os.getcwd()+"\\failedcases.txt"
+           with open(current_path,'w') as file:
+                file.write(str({'status':'success'})+"\n")
+           return {'status':'success','response':"written successfully"}
+        except Exception as e:
+            return {'status': 'failed', 'result': str(e)}
