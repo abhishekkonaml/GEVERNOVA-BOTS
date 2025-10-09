afdcb35bd2f3e7b967c060fb16c05c891ce7f4ba
Abhishek Kona <abhishek.kona@microland.com>
2025-10-09 17:13:21 +0530

    Update coreappapi.py

--- GEV_BOTS_Prod/action_plugins/coreappapi.py
+++ GEV_BOTS_Prod/action_plugins/coreappapi.py
@@ -4,7 +4,10 @@ import json
 class CoreApp:
     def coreapp_trigger(self,ticketNo,desc,org_assignment_group):
         url = "https://bots.intelligeni.gevernova.net/bots/apis/resolve"
-        
+        proxies={
+          'http_proxy' : '',
+          'https_proxy' : ''
+        }
         payload = json.dumps({
           "ticket": {
             "source": "servicenow-prod",
@@ -22,7 +25,7 @@ class CoreApp:
           'AuthToken': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODM5NzkyZTJkODIzOTVjYWFkZGRlNzkiLCJlbWFpbCI6ImlnYm90c2FwaXNhZG1pbkBnZXZlcm5vdmEuY29tIiwidGVuYW50IjoiZ2VfdmVybm92YSIsImlhdCI6MTc1NTUyMTI3OCwiZXhwIjoxNzg3MDU3Mjc4fQ.DGIGAMkcAYpxWBe9R-kmZPiZGgI2zH4vyn8iyN8aHAE'
         }
         try:
-            response = requests.request("POST", url, headers=headers, data=payload)
+            response = requests.request("POST", url, headers=headers, data=payload,proxies=proxies)
             if response.status_code==201:
                return response.json()
             else:
