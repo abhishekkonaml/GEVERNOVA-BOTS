9b29dba07e6ff03b4fbac26a31c7f05874af7223
Abhishek Kona <abhishek.kona@microland.com>
2025-10-09 17:14:38 +0530

    Update coreappapi.py

--- GEV_BOTS_Prod/action_plugins/coreappapi.py
+++ GEV_BOTS_Prod/action_plugins/coreappapi.py
@@ -25,7 +25,7 @@ class CoreApp:
           'AuthToken': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODM5NzkyZTJkODIzOTVjYWFkZGRlNzkiLCJlbWFpbCI6ImlnYm90c2FwaXNhZG1pbkBnZXZlcm5vdmEuY29tIiwidGVuYW50IjoiZ2VfdmVybm92YSIsImlhdCI6MTc1NTUyMTI3OCwiZXhwIjoxNzg3MDU3Mjc4fQ.DGIGAMkcAYpxWBe9R-kmZPiZGgI2zH4vyn8iyN8aHAE'
         }
         try:
-            response = requests.request("POST", url, headers=headers, data=payload,proxies=proxies)
+            response = requests.request("POST", url, headers=headers, data=payload,proxies=proxies,verify=False)
             if response.status_code==201:
                return response.json()
             else:
