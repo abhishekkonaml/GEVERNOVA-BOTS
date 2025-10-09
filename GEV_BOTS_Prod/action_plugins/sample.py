72659389b2cb952af84f3dc8eec40a8eaf8771be
Abhishek Kona <abhishek.kona@microland.com>
2025-10-09 21:48:47 +0530

    sample

---
+++ GEV_BOTS_Prod/action_plugins/sample.py
@@ -0,0 +1,7 @@
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+current_path=os.getcwd()+"\\failedcases.txt"
+
+with open(current_path,'a') as file:
+    file.write(str({'status':'success'})+"\n")
