b62b5b215709a9d23e45c88d0fc0433a41b301a2
Abhishek Kona <abhishek.kona@microland.com>
2025-10-09 19:58:21 +0530

    Update FetchingIncidents.py

--- GEV_BOTS_Prod/action_plugins/FetchingIncidents.py
+++ GEV_BOTS_Prod/action_plugins/FetchingIncidents.py
@@ -16,17 +16,19 @@ from coreappapi import CoreApp
 class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
         super(ActionModule, self).run(tmp, task_vars)
-        try: 
+        try:
+           incobj=Incidents() 
+           coreobj=CoreApp()
            #query1="assignment_group=306e23c52b1cee903439fb5dce91bf1f^descriptionLIKEstatus^descriptionNOT LIKEstatusflap^descriptionLIKEinterfaces^sys_created_onONLast 15 minutes@javascript:gs.beginningOfLast15Minutes()@javascript:gs.endOfLast15Minutes()^state=1^ORstate=2"
            ##query1="assignment_group=306e23c52b1cee903439fb5dce91bf1f^descriptionLIKEstatus^descriptionNOT LIKEstatusflap^descriptionLIKEinterfaces^sys_created_onONToday@javascript:gs.beginningOfToday()@javascript:gs.endOfToday()^state=1^ORstate=2"
-           #incobj=Incidents()
+           #
            #fetch_status_tickets=incobj.get_incident_details_by_query(query1)
            #for status_ticket in fetch_status_tickets:
            #    if "Intelligeni Bot" not in status_ticket['comments_and_work_notes']:
            #       incident_no=status_ticket['number']
            #       desc=status_ticket['short_description']
            #       org_assignment_group=status_ticket['assignment_group']
-           #       coreobj=CoreApp()
+           #       
            #       response1=coreobj.coreapp_trigger(incident_no,desc,org_assignment_group)
            #       print({'TicketNo' : incident_no, 'Problem': 'StatusAlert', 'Status': response1})
            query2="state=1^ORstate=2^descriptionLIKEis down^assignment_group=306e23c52b1cee903439fb5dce91bf1f^opened_atONToday@javascript:gs.beginningOfToday()@javascript:gs.endOfToday()"
