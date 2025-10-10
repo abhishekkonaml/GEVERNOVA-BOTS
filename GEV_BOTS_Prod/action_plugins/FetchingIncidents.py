8fe671504704ca489ec506319943752fc09ff54a
Abhishek Kona <abhishek.kona@microland.com>
2025-10-10 17:45:42 +0530

    Update FetchingIncidents.py

--- GEV_BOTS_Prod/action_plugins/FetchingIncidents.py
+++ GEV_BOTS_Prod/action_plugins/FetchingIncidents.py
@@ -31,10 +31,11 @@ class ActionModule(ActionBase):
                   
                   response1=coreobj.coreapp_trigger(incident_no,desc,org_assignment_group)
                   print({'TicketNo' : incident_no, 'Problem': 'StatusAlert', 'Status': response1})
-           query2="state=1^ORstate=2^short_descriptionLIKEis down^assignment_group=306e23c52b1cee903439fb5dce91bf1f^sys_created_onONLast 15 minutes@javascript:gs.beginningOfLast15Minutes()@javascript:gs.endOfLast15Minutes()"
+           #query2="state=1^ORstate=2^short_descriptionLIKEis down^assignment_group=306e23c52b1cee903439fb5dce91bf1f^sys_created_onONLast 15 minutes@javascript:gs.beginningOfLast15Minutes()@javascript:gs.endOfLast15Minutes()"
+           query2="state=2^ORstate=1^descriptionLIKEis down^assignment_group=306e23c52b1cee903439fb5dce91bf1f^sys_created_onONToday@javascript:gs.beginningOfToday()@javascript:gs.endOfToday()"
            fetch_idleinterval_tickets=incobj.get_incident_details_by_query(query2)
            for idleinterval_ticket in fetch_idleinterval_tickets:
-               if "Intelligeni Bot" not in idleinterval_ticket['comments_and_work_notes']:
+               if "Intelligeni Bot" not in idleinterval_ticket['comments_and_work_notes'] and if "GEVINC0055402" == idleinterval_ticket['number']:
                   incident_no=idleinterval_ticket['number']
                   desc=idleinterval_ticket['short_description']
                   org_assignment_group=idleinterval_ticket['assignment_group']
