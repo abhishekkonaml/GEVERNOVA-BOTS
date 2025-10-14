b9d0284d4f4ac6cec4145104186abda80f309249
Abhishek Kona <abhishek.kona@microland.com>
2025-10-14 22:03:54 +0530

    Update stalling_ticket_reassignment.py

--- GEV_BOTS_Prod/action_plugins/stalling_ticket_reassignment.py
+++ GEV_BOTS_Prod/action_plugins/stalling_ticket_reassignment.py
@@ -4,7 +4,7 @@ from ansible.plugins.action import ActionBase
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
+import warnings
 from GEV_Prod_Servicenow import Incidents
 
 incobj=Incidents()
