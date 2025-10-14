757d852a5f55145eba604c8462e5aa88a518bc54
Abhishek Kona <abhishek.kona@microland.com>
2025-10-14 23:18:16 +0530

    Update stalling_ticket_reassignment.py

--- GEV_BOTS_Prod/action_plugins/stalling_ticket_reassignment.py
+++ GEV_BOTS_Prod/action_plugins/stalling_ticket_reassignment.py
@@ -14,24 +14,29 @@ class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
         super(ActionModule, self).run(tmp, task_vars)
         try: 
-            #query1="assignment_group=609fb9273b426690e2d5cbc964e45a7a^state!=6^state!=7"
+            query1="assignment_group=609fb9273b426690e2d5cbc964e45a7a^state!=6^state!=7"
             
-            query1="assignment_group=306e23c52b1cee903439fb5dce91bf1f^short_descriptionLIKEstatusflap^state=1^ORstate=2"
+            #query1="assignment_group=306e23c52b1cee903439fb5dce91bf1f^short_descriptionLIKEstatusflap^state=1^ORstate=2"
             incobj=Incidents()
             fetch_stalled_tickets=incobj.get_incident_details_by_query(query1)
             for stalled_ticket in fetch_stalled_tickets:
                 Number=stalled_ticket['number']
                 group='HQ CTO Network Enterprise Site Support'
                 openedAt=stalled_ticket['sys_created_on']
-                print(Number,repr(openedAt))
-                current_utc_time = datetime.now(timezone.utc)
-                tickettime=datetime.strptime(openedAt,"%m-%d-%Y %I:%M:%S %p")
-                utctime=datetime.strftime(current_utc_time,"%m-%d-%Y %I:%M:%S %p")
-                utc_time_now=datetime.strptime(utctime,"%m-%d-%Y %I:%M:%S %p")
-                time_difference=(utc_time_now-tickettime).total_seconds()
-                if time_difference>3600:
-                    print(Number,group)
-                    print("=-=-"*25)
+                #print(Number,repr(openedAt))
+                try:
+                    current_utc_time = datetime.now(timezone.utc)
+                    tickettime=datetime.strptime(openedAt,"%m-%d-%Y %I:%M:%S %p")
+                    utctime=datetime.strftime(current_utc_time,"%m-%d-%Y %I:%M:%S %p")
+                    utc_time_now=datetime.strptime(utctime,"%m-%d-%Y %I:%M:%S %p")
+                    time_difference=(utc_time_now-tickettime).total_seconds()
+                    if time_difference>3600:
+                        #print(Number,group)
+                        #print("=-=-"*25)
+                        print(Number,time_difference)
+                        print(incobj.reassignment(Number,group))
+                except Exception as e:
+                       pass
             return {'status': 'success', 'response': 'Stalled tickets reassigned successfully'}
         except Exception as e:
             return {'status': 'failed','response':str(e)}
