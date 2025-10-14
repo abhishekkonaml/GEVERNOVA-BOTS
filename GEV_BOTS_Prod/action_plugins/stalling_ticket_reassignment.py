9aedb829785dc7fbfc7aef7ac90f41c9c10b6419
Abhishek Kona <abhishek.kona@microland.com>
2025-10-14 22:00:44 +0530

    stall

--- GEV_BOTS_Prod/action_plugins/stalling_ticket_reassignment.py
+++ GEV_BOTS_Prod/action_plugins/stalling_ticket_reassignment.py
@@ -14,13 +14,15 @@ class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
         super(ActionModule, self).run(tmp, task_vars)
         try: 
-            query1="assignment_group=609fb9273b426690e2d5cbc964e45a7a^state!=6^state!=7"
+            #query1="assignment_group=609fb9273b426690e2d5cbc964e45a7a^state!=6^state!=7"
+            query1="assignment_group=609fb9273b426690e2d5cbc964e45a7a^state=6^ORstate=7"
             incobj=Incidents()
             fetch_stalled_tickets=incobj.get_incident_details_by_query(query1)
             for stalled_ticket in fetch_stalled_tickets:
                 Number=stalled_ticket['number']
                 group='HQ CTO Network Enterprise Site Support'
                 openedAt=stalled_ticket['sys_created_on']
-                
+                print(Number,openedAt)
+                return {'status': 'success', 'reponse': 'Stalled tickets reassigned successfully'}
         except Exception as e:
             return {'status': 'failed','response':str(e)}
