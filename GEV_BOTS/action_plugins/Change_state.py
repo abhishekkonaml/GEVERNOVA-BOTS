a55de603f6a5079ad1e40f30542e80f3773b9f65
aishwarya-shet <aishwarya.shet@microland.com>
2025-10-27 19:39:27 +0530

    Update Change_state.py

--- GEV_BOTS/action_plugins/Change_state.py
+++ GEV_BOTS/action_plugins/Change_state.py
@@ -26,7 +26,9 @@ class ActionModule(ActionBase):
                sys_id = chobj.get_ci_affected_details_change(ch_number)
                sys = sys_id['result'][0]['sys_id']
                tasks_data = chobj.get_tasks_by_change_sys_id(sys)
-               print(tasks_data)
+               for i in tasks_data:
+                   print(i['change_task_type'])
+            
 
                
                
