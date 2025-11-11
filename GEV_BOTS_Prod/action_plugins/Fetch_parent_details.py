9d5650de1d57aabbc8adcc5877c3b2a699d8d257
aishwarya-shet <aishwarya.shet@microland.com>
2025-11-11 17:44:45 +0530

    update

---
+++ GEV_BOTS_Prod/action_plugins/Fetch_parent_details.py
@@ -0,0 +1,34 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+from ansible.plugins.action import ActionBase
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import warnings
+from GEV_Prod_Servicenow import Incidents
+from datetime import datetime
+import pytz
+incobj=Incidents()
+warnings.filterwarnings("ignore")
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+        super(ActionModule, self).run(tmp, task_vars)
+        try: 
+            
+            Query=self._task.args["query1"]
+            incobj=Incidents()
+            fetch_all_tickets_with_parent=incobj.get_incident_details_by_query(Query)
+            
+            Incident_list=[]
+            for incident_ticket in fetch_all_tickets_with_parent:
+                Number=incident_ticket['number']
+                Incident_list.append(Number)
+            if(len(Incident_list) == 0):
+                result='No device silencing tickets'
+            else:
+                result=Incident_list
+    
+            return {'status': 'success', 'response': result}
+        except Exception as e:
+            return {'status': 'failed','response':str(e)}
