93d8cb8e17742baf6b9050f6f478dfbb99ee15b1
aishwarya-shet <aishwarya.shet@microland.com>
2025-11-11 18:27:32 +0530

    Update Fetch_Parent_details_and_update.py

--- GEV_BOTS_Prod/action_plugins/Fetch_Parent_details_and_update.py
+++ GEV_BOTS_Prod/action_plugins/Fetch_Parent_details_and_update.py
@@ -20,7 +20,7 @@ class ActionModule(ActionBase):
             incobj=Incidents()
             fetch_parent_details=incobj.get_parent_details_by_number(Number)
             
-            op=fetch_parent_details
+            op=fetch_parent_details['result'][0]['short_description']
             return {'status': 'success', 'response': op}
         except Exception as e:
             return {'status': 'failed','response':str(e)}
