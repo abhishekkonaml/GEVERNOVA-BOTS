c3f831b4c47d9fb802350631504fb5b132035f78
aishwarya-shet <aishwarya.shet@microland.com>
2025-11-11 18:33:54 +0530

    Update Fetch_Parent_details_and_update.py

--- GEV_BOTS_Prod/action_plugins/Fetch_Parent_details_and_update.py
+++ GEV_BOTS_Prod/action_plugins/Fetch_Parent_details_and_update.py
@@ -19,8 +19,8 @@ class ActionModule(ActionBase):
             Number=self._task.args["INC_Number"]
             incobj=Incidents()
             fetch_parent_details=incobj.get_parent_details_by_number(Number)
-            op=fetch_parent_details
-            print(type(fetch_parent_details))
+            op=fetch_parent_details[0]['short_description']
+            print(type(op))
             return {'status': 'success', 'response': op}
         except Exception as e:
             return {'status': 'failed','response':str(e)}
