0fa1c5506aff61adefe9c3b019e4acc3ffb5335c
aishwarya-shet <aishwarya.shet@microland.com>
2025-11-11 18:06:00 +0530

    update

---
+++ GEV_BOTS_Prod/action_plugins/Fetch_Parent_details_and_update.py
@@ -0,0 +1,26 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+from ansible.plugins.action import ActionBase
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import warnings
+from GEV_Prod_Servicenow import Incidents
+from datetime import datetime
+import pytz
+incobj=Incidents()
+warnings.filterwarnings("ignore")
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+        super(ActionModule, self).run(tmp, task_vars)
+        try: 
+            
+            Number=self._task.args["INC_Number"]
+            incobj=Incidents()
+            fetch_parent_details=incobj.get_parent_details_by_number(Number)
+            
+            result=fetch_parent_details
+            return {'status': 'success', 'response': result}
+        except Exception as e:
+            return {'status': 'failed','response':str(e)}
--- GEV_BOTS_Prod/action_plugins/GEV_Prod_Servicenow.py
+++ GEV_BOTS_Prod/action_plugins/GEV_Prod_Servicenow.py
@@ -184,6 +184,23 @@ class Incidents:
                   return ("Failed - {}".format(response.json()['error']['message']))
           except Exception as e:
                   return ("Failed - Something went wrong - {}".format(str(e)))
+    def get_parent_details_by_number(self,number):
+          url1="https://api.gevernova.com/servicenow_incident/query/number={}?sso=503438685".format(number)
+          token='Bearer {}'.format(self.access_token)
+          headers = {
+               'Authorization': token,
+               'tradingPartner': 'com.microland.intelligenie',
+               'Content-Type': 'application/json'
+              }
+          try: 
+               response = requests.request("GET", url1, headers=headers,verify=False)
+               if response.status_code == 200:
+                  return response.json()['result']
+               else:
+                  return ("Failed - {}".format(response.json()['error']['message']))
+          except Exception as e:
+                  return ("Failed - Something went wrong - {}".format(str(e)))        
+        
     def get_incident_details_by_query(self,query):
           url1="https://api.gevernova.com/servicenow_incident/query/{}?sso=503437104".format(query)
           token='Bearer {}'.format(self.access_token)
