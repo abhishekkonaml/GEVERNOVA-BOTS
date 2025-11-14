aaa59ada7f45442840be8daf39f7c5901a33d60b
aishwarya-shet <aishwarya.shet@microland.com>
2025-11-14 22:38:25 +0530

    Update Change_PTR.py

--- GEV_BOTS/action_plugins/Change_PTR.py
+++ GEV_BOTS/action_plugins/Change_PTR.py
@@ -5,7 +5,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import warnings
-from GEV_DEV_Servicenow import Incidents,CMDB
+from GEV_Stage_Servicenow import Incidents,CMDB
 from datetime import datetime
 import pytz
 incobj=Incidents()
