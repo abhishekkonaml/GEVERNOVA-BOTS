dbcf28ff94ed0b4a804b94fd7cfa80302071a634
aishwarya-shet <aishwarya.shet@microland.com>
2025-11-18 14:02:07 +0530

    update

--- GEV_BOTS/action_plugins/Change_state.py
+++ GEV_BOTS/action_plugins/Change_state.py
@@ -4,7 +4,8 @@ from ansible.plugins.action import ActionBase
 import sys
 import os
 import warnings
-from GEV_Prod_Servicenow import CMDB
+#from GEV_Prod_Servicenow import CMDB
+from GEV_Stage_Servicenow import CMDB
 import math
 
 warnings.filterwarnings("ignore")
--- GEV_BOTS/action_plugins/Prevalidation.py
+++ GEV_BOTS/action_plugins/Prevalidation.py
@@ -8,7 +8,8 @@ import base64
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from BtoBtransaction import GrabIncidents
-from GEV_Prod_Servicenow import CMDB
+#from GEV_Prod_Servicenow import CMDB
+from GEV_Stage_Servicenow import CMDB
 from awx_trigger import AWX_Trigger
 warnings.filterwarnings("ignore") 
 
