4e6bcfb4a48ffb36e4ee0a99c9bcb95d2c49738e
Abhishek Kona <abhishek.kona@microland.com>
2026-01-05 16:16:51 +0530

    password

--- GEV_BOTS_Prod/action_plugins/CommandExecution.py
+++ GEV_BOTS_Prod/action_plugins/CommandExecution.py
@@ -33,8 +33,8 @@ class ActionModule(ActionBase):
             #cisco_username=decoding1.decoding1('NTA0MDIwMzIw')
             #cisco_password=decoding1.decoding1('V2hpejIwMTJDMTBzZQ==')
             #nbassure credentials
-            cisco_username=decoding1.decoding1('NTA0MDIwMzIw')
-            cisco_password=decoding1.decoding1('TmJAc3N1cmU4Ny1STw==')
+            cisco_username=decoding1.decoding1('NTA0MDE4ODg3')
+            cisco_password=decoding1.decoding1('QjB0czg4LVJP')
             if "gdn" not in hostname1:
                hostname=hostname1+".gdn.ge.com"
             else:
