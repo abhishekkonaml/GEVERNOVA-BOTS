c4f80880776e67d5c7af105f96c7ffc09fb5546e
aishwarya-shet <aishwarya.shet@microland.com>
2026-06-11 18:46:59 +0530

    Update Incident_details_extraction.py

--- GEV_BOTS_STAGE/action_plugins/Incident_details_extraction.py
+++ GEV_BOTS_STAGE/action_plugins/Incident_details_extraction.py
@@ -9,7 +9,7 @@ import re
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 
-from GEV_DEV_Servicenow import Incidents
+from GEV_Stage_Servicenow import Incidents
 
 warnings.filterwarnings("ignore")
 
