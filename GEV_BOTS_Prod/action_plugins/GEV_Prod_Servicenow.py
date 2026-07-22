71b41cc70379075861730661f1743b7c88ac6dba
aishwarya-shet <aishwarya.shet@microland.com>
2026-07-22 16:26:24 +0530

    update

--- GEV_BOTS_Prod/action_plugins/GEV_Prod_Servicenow.py
+++ GEV_BOTS_Prod/action_plugins/GEV_Prod_Servicenow.py
@@ -143,6 +143,35 @@ class Incidents:
                return ("Failed - {}".format(response.json()))
         except Exception as e:
                return ("Failed - Something went wrong - {}".format(str(e)))
+        
+    def Reassign_to_renewables_Engineer(self,incidentnumber):
+        token='Bearer {}'.format(self.access_token)
+        url1="https://api.gevernova.com/servicenow_incident/" 
+        payload = json.dumps({
+                              "update": {
+                                "partnerInfo": {
+                                  "name": "com.microland.intelligenie",
+                                  "externalRecord": ""
+                                },
+                                "number": incidentnumber,
+                                "work_notes": "Unable to trobleshoot, Reassigning ticket to engineer",
+                                "assigned_to": "502742160",
+                           
+                              }
+                            })
+        headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': token,
+                    'Cookie': 'glide_user_route=glide.ab96de1815d3b1b5951c142924425060'
+                  }
+        try: 
+            response = requests.request("PUT", url1, headers=headers,verify=False,data=payload)
+            if response.status_code == 201:
+               return (response.json()['result'])
+            else:
+               return ("Failed - {}".format(response.json()))
+        except Exception as e:
+               return ("Failed - Something went wrong - {}".format(str(e)))
     def update_notes(self,incidentnumber,work_notes):
         token='Bearer {}'.format(self.access_token)
         url1="https://api.gevernova.com/servicenow_incident/" 
