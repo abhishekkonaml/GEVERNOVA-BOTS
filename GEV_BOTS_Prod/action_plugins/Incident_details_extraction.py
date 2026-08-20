ee58bd65c810405f9eaba29a822218ab8093a011
Abhishek Kona <abhishek.kona@microland.com>
2026-08-20 19:06:34 +0530

    Update Incident_details_extraction.py

--- GEV_BOTS_Prod/action_plugins/Incident_details_extraction.py
+++ GEV_BOTS_Prod/action_plugins/Incident_details_extraction.py
@@ -24,6 +24,8 @@ class ActionModule(ActionBase):
             
             incobj=Incidents()
             #print(incobj.assigned_to_bot(incident_no))
+            print(incident_no)
+            print("=--=="*25)
             incident_details=incobj.get_incident_details_by_incident_number(incident_no)
             print(incident_details)
             print("----------"*25)
