053d69dbabc5df3513ec21618beeaac36e2d1201
aishwarya-shet <aishwarya.shet@microland.com>
2026-08-31 19:29:06 +0530

    Update Incident_details_extraction.py

--- GEV_BOTS_STAGE/action_plugins/Incident_details_extraction.py
+++ GEV_BOTS_STAGE/action_plugins/Incident_details_extraction.py
@@ -105,7 +105,8 @@ class ActionModule(ActionBase):
                   return {'status':'success','hostname':hostname,'description':description,'botname': 'Node down'}
                
                elif 'temperature sensors' in description.lower():
-                  hostname='POWfeSINnoida51a'
+                  #hostname='POWfeSINnoida51a'
+                  hostname='POWfeSINnoida51b'
                   #hostname=incident_details[0]['cmdb_ci']
                   if hostname == '':
                      return {'status': 'failed','reason': 'Bot failed to extract the details for execution'}
